63c8857bea0285f0af63656890ec754739bf05b3
Janobprogrammer <121550433+Janobprogrammer@users.noreply.github.com>
2026-02-13 11:37:24 +0500

    Update asgi.py
    ....

--- config/asgi.py
+++ config/asgi.py
@@ -1,11 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
-from dotenv import load_dotenv
 
-load_dotenv()
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE'))
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
