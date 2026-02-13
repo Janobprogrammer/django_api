70ab8a1e351212591b4f6cded57920ac38d32648
Janobprogrammer <121550433+Janobprogrammer@users.noreply.github.com>
2026-02-13 11:37:01 +0500

    Update wsgi.py
    ..

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,10 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
